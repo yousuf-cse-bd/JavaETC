de01249a34b64359545b839e8ea6d5849ef2b399
yousuf-cse-bd <yousuf.cse17@gmail.com>
2025-03-07 14:08:56 +0600

    Java takes faction value as double data-typed

---
+++ DefaultDouble.java
@@ -0,0 +1,15 @@
+package JavaETC;
+//Java takes faction value as double data-typed
+
+public class DefaultDouble {
+    public static void main(String[] args) {
+        double myDouble = Math.acos(-1); //PI value
+        System.out.println(myDouble);
+
+        float myFloat = (float) Math.acos(-1);
+        System.out.println(myFloat);
+
+        float tempFloat = 3.1425927F;
+        System.out.println(tempFloat);
+    }
+}
